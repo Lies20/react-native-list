0e5e3003adb0601e2e350dba3daf9183e7ffcc6c
lies93 <hocini.lies@gmail.com>
2022-04-06 14:26:47 +0200

    day 2 done

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, FlatList,} from 'react-native';
+import axios from 'axios';
+
 
 export default function App() {
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+
+  const [countries,setCountries] = useState([]);
+
+  useEffect(()=>{
+    axios.get("https://restcountries.com/v3.1/all").then(resultat=>{
+         console.log("resultat",resultat.data[2].name.common)
+         setCountries(resultat.data)
+      })
+    },
+    [])
+  
+
+      const renderItem=({ item }) => {
+      console.log("item",item)
+        return (
+        <View>
+          <Text>{item.name.common}</Text>
+        </View>
+      );
+    }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <FlatList data = {countries}
+    renderItem ={renderItem}/> 
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
